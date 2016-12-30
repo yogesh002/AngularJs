ca6c34e566036984cf43f57af725c95ef9045635
Yogesh <ygh002@gmail.com>
2016-12-29 18:21:36 -0600

    arranging files under a folder named exampleOne

---
+++ ExampleOne/app.js
@@ -0,0 +1,16 @@
+/**
+ * @author Yogesh Ghimire 
+ */
+
+(function(){
+    'use strict';
+angular.module("myFirstAngularApp", [])
+
+.controller("myFirstAngularController", function($scope){
+    $scope.name = "Yogesh";    
+    $scope.askName = function(){
+        return "What is your name?"
+        }    
+    });
+})();
+
